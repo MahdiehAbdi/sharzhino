f84fa4ac1d561402f460c0137e4fdbc141d30909
SheydaAtaei <132059227+SheydaAtaei@users.noreply.github.com>
2023-06-21 01:59:03 -0700

    Add files via upload

--- app/src/main/java/com/example/sharzhino/buy_page.java
+++ app/src/main/java/com/example/sharzhino/buy_page.java
@@ -17,6 +17,10 @@ public class buy_page extends AppCompatActivity {
     private Button rightel_btn;
     private Button payment_btn1;
     private Button back_btn1;
+    private String GetEditText1;
+    private String GetEditText2;
+    private String url ="https://shop.mci.ir/charge";
+    private WebView wv1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,13 +32,45 @@ public class buy_page extends AppCompatActivity {
         rightel_btn=findViewById(R.id.rightel_btn);
         payment_btn1=findViewById(R.id.payment_btn1);
         back_btn1=findViewById(R.id.back_btn1);
+        wv1=(WebView)findViewById(R.id.webView);
+        wv1.setWebViewClient(new MyBrowser());
     }
+GetEditText1 = amount_et.getText().toString();
+if(TextUtils.isEmpty(GetEditText1)){
+
+}
+GetEditText2 = mobile_et.getText().toString();
+if(TextUtils.isEmpty(GetEditText2)){
+
+}
     public void click1(View v1) {
         if (v1.getId() == R.id.payment_btn1) {
-
+            wv1.getSettings().setLoadsImagesAutomatically(true);
+            wv1.getSettings().setJavaScriptEnabled(true);
+            wv1.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+            wv1.loadUrl(url);
+        } else if (v1.getId() == R.id.MCI_btn) {
+            MCI_btn.setText("انتخاب شد");
+            irancell_btn.setText("ایرانسل");
+            rightel_btn.setText("رایتل");
+        } else if (v1.getId() == R.id.irancell_btn) {
+            MCI_btn.setText("همراه اول");
+            irancell_btn.setText("انتخاب شد");
+            rightel_btn.setText("رایتل");
+        } else if (v1.getId() == R.id.rightel_btn) {
+            MCI_btn.setText("همراه اول");
+            irancell_btn.setText("ایرانسل");
+            rightel_btn.setText("انتخاب شد");
         } else if (v1.getId() == R.id.back_btn1) {
-            Intent intent1 = new Intent(buy_page.this, main_page.class);
-            startActivity(intent1);
+            Intent intent3 = new Intent(buy_page.this, main_page.class);
+            startActivity(intent3);
         }
     }
+private class MyBrowser extends WebViewClient {
+      @Override
+      public boolean shouldOverrideUrlLoading(WebView view, String url) {
+         view.loadUrl(url);
+         return true;
+}
+}
 }
